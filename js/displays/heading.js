e57345f7f1a5f56e073f2de52fbb08148b35a6f0
Grady Morgan <gradymorgan@gmail.com>
2014-03-12 21:51:02 -0700

    initial commit, yo

---
+++ js/displays/heading.js
@@ -0,0 +1,102 @@
+var heading_template = '<canvas class="ticker" width="1000px" height="20px"></canvas>\
+            <div class="label">HDG</div>\
+            <div class="value"></div>\
+            <!-- small decimal -->\
+            <div class="units">°</div>';
+
+var headingView = Backbone.View.extend({
+    tagName: "div",
+    className: "section cf hdg",
+    template: heading_template,
+    initialize: function() {
+        this.listenTo(this.model, "change", this.render);
+        this.lastRender = 0;
+        this.$el.html(this.template);
+    },
+
+    render: function() {
+        var now = new Date().getTime();
+
+        if (now - this.lastRender > 200) {
+            var displayVal = this.model.get('hdg');
+            this.$('.value').text(displayVal.toFixed(1));
+            this.lastRender = now;
+        }
+        this.drawTicker();
+        
+        return this;
+    },
+
+    drawTicker: function() {
+        var ticker = this.$('.ticker')[0];
+        var context = ticker.getContext("2d");
+
+        //setup
+        context.save();
+        context.fillStyle = '#fff';
+        context.fillRect(0,0,ticker.width, ticker.height);
+
+
+        //draw ticks
+        context.strokeStyle = "#000";
+
+        //width 10deg
+        var ticks = 12;
+        var xscale = ticker.width / ticks;
+
+        var currentHdg = this.model.get('hdg');
+        for ( var i=Math.floor(currentHdg-ticks/2.0); i <= Math.ceil(currentHdg+1+ticks/2.0); i++ ) {
+            var x = parseInt((i - (currentHdg - 5.0)) * xscale);
+
+            //make every 10th line thicker
+            if ( (i-1) % 5 == 0 ) {
+                context.lineWidth = 3;
+            }
+            else if ( (i-1) % 5 == 0 ) {
+                context.lineWidth = 3;
+                x -= .5;
+            }
+            else 
+                context.lineWidth = 2;
+
+            if ( (i-1) % 10 == 0 )
+                context.strokeStyle = '#900';
+            else
+                context.strokeStyle = '#000';
+            
+            context.beginPath();
+            context.moveTo(x, 0);
+            context.lineTo(x, ticker.height);
+            context.stroke();
+        }
+
+        //draw center pointer
+        context.lineWidth = 1;
+        context.strokeStyle = "rgba(0, 0, 255, 0.75)";
+        context.beginPath();
+        context.moveTo(ticker.width/2+.5, 0);
+        context.lineTo(ticker.width/2+.5, ticker.height);
+        context.stroke();
+
+        context.strokeStyle = "#00f";
+        context.fillStyle = "#00f";
+        
+        context.beginPath();
+        context.moveTo(ticker.width/2+.5, 6);
+        context.lineTo(ticker.width/2-8, 0);
+        context.lineTo(ticker.width/2+9, 0);
+        
+        context.closePath();
+        context.fill();
+
+        // context.beginPath();
+        // context.moveTo(ticker.width/2+.5, ticker.height-4);
+        // context.lineTo(ticker.width/2-3.5, ticker.height);
+        // context.lineTo(ticker.width/2+3.5, ticker.height);
+        // context.closePath();
+        // context.fill();
+
+
+        context.restore();
+    }
+});
---
+++ js/displays/speed.js
@@ -0,0 +1,72 @@
+var speed_template = '<div class="label">SPD</div>\
+            <div class="value"></div>\
+            <div class="units">kts</div>\
+            <canvas class="eq" width="40px" height="100px"></div>';
+
+var speedView = Backbone.View.extend({
+    tagName: "div",
+    className: "section cf spd",
+    template: speed_template,
+    initialize: function() {
+        this.listenTo(this.model, "change", this.render);
+        this.lastRender = new Date().getTime();
+        this.$el.html(this.template);
+
+        this.last = 0;
+    },
+
+    render: function() {
+        var now = new Date().getTime();
+        var speed = this.model.get('speed');
+
+        if (now - this.lastRender > 200) {
+            this.$('.value').text(speed.toFixed(1));
+            this.lastRender = now;
+        }
+        
+
+        this.drawEQ(speed - this.last);
+
+        this.last = speed;
+
+        return this;
+    },
+
+    //TODO: better, consider time since
+    drawEQ: function(accel) {
+        var eq = this.$('.eq')[0];
+        var context = eq.getContext("2d");
+
+        //setup
+        context.save();
+        context.fillStyle = '#fff';
+        context.fillRect(0,0,eq.width, eq.height);
+
+        var scaley = eq.height / 2.0;
+
+        var mid = eq.height / 2;
+        var signal = mid - (accel * scaley);
+
+        context.strokeStyle = '#00f';
+        context.fillStyle = '#00f';
+        
+        context.beginPath();
+        context.moveTo(0, mid);
+        context.lineTo(0, signal);
+        context.lineTo(eq.width, signal);
+        context.lineTo(eq.width, mid);
+        
+        context.closePath();
+        context.fill();
+
+        //draw center line
+        context.lineWidth = 1;
+        context.strokeStyle = "#000";
+        context.beginPath();
+        context.moveTo(0, mid);
+        context.lineTo(eq.width, mid);
+        context.stroke();
+
+        context.restore();
+    }
+});
